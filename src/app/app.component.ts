5b90a837a2c72ee98a251679f93776b0581a9cfd
Shivam Patel <shivampatel251097@gmail.com>
2020-09-29 16:55:08 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assignment4-Game-Control';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onIntervalFired(firedNumber: number){
+    console.log(firedNumber);
+    if(firedNumber % 2 !=0){
+      this.oddNumbers.push(firedNumber);
+    }
+    else{
+      this.evenNumbers.push(firedNumber);
+    }
+
+  }
 }
